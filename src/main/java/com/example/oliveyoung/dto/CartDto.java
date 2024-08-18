562e6aeef206ae710bdb23eb27b9423e88818b9d
jaebinGit <tnwoql327@gmail.com>
2024-08-18 21:10:46 +0900

    Remove : msa를 위한 ddd설계로 변경으로 삭제

--- src/main/java/com/example/oliveyoung/dto/CartDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.oliveyoung.dto;
-
-public class CartDto {
-}
--- src/main/java/com/example/oliveyoung/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.oliveyoung.repository;
-
-import com.example.oliveyoung.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
