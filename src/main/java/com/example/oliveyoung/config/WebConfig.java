e53d7d8914cfcbc03d59d3feb476938315217e90
jaebinGit <tnwoql327@gmail.com>
2024-08-23 14:28:22 +0900

    Refactor : redis 이용 refresh token 관리

--- src/main/java/com/example/oliveyoung/config/WebConfig.java
+++ src/main/java/com/example/oliveyoung/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // API 경로를 지정
-                .allowedOrigins("https://www.hachwimu.com")  // React 애플리케이션이 배포된 도메인 (프론트엔드 도메인)
+                .allowedOrigins("https://www.hachwimu.com", "http://www.hachwimu.com:3000", "http://hachwimu.com")  // React 애플리케이션이 배포된 도메인 (프론트엔드 도메인)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // allowCredentials가 true일 경우, '*' 대신 구체적인 도메인을 지정해야 함
