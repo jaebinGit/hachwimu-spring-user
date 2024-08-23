9e46331ef6ab047dcfb8bde50cd76341ab6ebc7e
jaebinGit <tnwoql327@gmail.com>
2024-08-23 11:11:07 +0900

    Refactor : 인증헤더 포함으로 변경

--- src/main/java/com/example/oliveyoung/config/WebConfig.java
+++ src/main/java/com/example/oliveyoung/config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")  // 모든 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false); // allowCredentials가 true일 경우 allowedOrigins에 '*'를 사용할 수 없습니다.
+                .allowCredentials(true); // allowCredentials가 true일 경우 allowedOrigins에 '*'를 사용할 수 없습니다.
     }
 }
