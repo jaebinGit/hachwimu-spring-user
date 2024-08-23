def1a0718e2c885fd953829c66174a82cbbfdaac
jaebinGit <tnwoql327@gmail.com>
2024-08-23 23:05:57 +0900

    Refactor : 인증 경로 수정

--- src/main/java/com/example/oliveyoung/config/SecurityConfig.java
+++ src/main/java/com/example/oliveyoung/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/**", "/product/**").permitAll()
+                        .requestMatchers("/user/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
