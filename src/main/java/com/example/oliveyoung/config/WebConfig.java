4e3e7bcb4fe2202977cd223ab15b41884384f2e3
jaebinGit <tnwoql327@gmail.com>
2024-08-23 23:33:12 +0900

    Refactor : 인증 경로 수정

--- src/main/java/com/example/oliveyoung/config/WebConfig.java
+++ src/main/java/com/example/oliveyoung/config/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // API 경로를 지정
-                .allowedOrigins("*")
+                .allowedOrigins("https://www.hachwimu.com\", \"https://api.hachwimu.com\"")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false);  // 자격 증명을 허용 (쿠키, 인증 헤더 전송)
+                .allowCredentials(true);  // 자격 증명을 허용 (쿠키, 인증 헤더 전송)
     }
 }
