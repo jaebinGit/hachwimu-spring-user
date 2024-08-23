d9eb772fb91a7670f223c14d5c457eb787033a07
jaebinGit <tnwoql327@gmail.com>
2024-08-23 15:48:25 +0900

    Docs : test

--- src/main/java/com/example/oliveyoung/config/SecurityConfig.java
+++ src/main/java/com/example/oliveyoung/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/**","/health","/products").permitAll()
+                        .requestMatchers("/user/**","/health/**","/products/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
