d2cdc0040ad3ba424c93d52c128f03859b01ba6d
jaebinGit <tnwoql327@gmail.com>
2024-08-23 11:56:10 +0900

    Docs : test

--- src/main/java/com/example/oliveyoung/config/WebConfig.java
+++ src/main/java/com/example/oliveyoung/config/WebConfig.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("https://api.hachwimu.com")
-                .allowedOrigins("")  // 모든 도메인 허용
+        registry.addMapping("/**")  // API 경로를 지정
+                .allowedOrigins("https://www.hachwimu.com")  // React 애플리케이션이 배포된 도메인 (프론트엔드 도메인)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true); // allowCredentials가 true일 경우 allowedOrigins에 '*'를 사용할 수 없습니다.
+                .allowCredentials(true); // allowCredentials가 true일 경우, '*' 대신 구체적인 도메인을 지정해야 함
     }
 }
