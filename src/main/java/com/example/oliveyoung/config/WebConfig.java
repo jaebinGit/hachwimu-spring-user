4ddef77d31fa087ff7585f32800a3f4380c0f54c
jaebinGit <tnwoql327@gmail.com>
2024-08-23 15:31:18 +0900

    Docs : test

--- src/main/java/com/example/oliveyoung/config/WebConfig.java
+++ src/main/java/com/example/oliveyoung/config/WebConfig.java
@@ -9,21 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // API 경로를 지정
-                .allowedOrigins(
-                        "https://www.hachwimu.com",   // 프론트엔드 배포 도메인
-                        "http://www.hachwimu.com:3000",    // HTTP 프론트엔드 도메인
-                        "http://www.hachwimu.com",
-                        "https://api.hachwimu.com",   // API 서버 도메인
-                        "http://api.hachwimu.com"     // HTTP로 API 요청
-                )
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);  // 자격 증명을 허용 (쿠키, 인증 헤더 전송)
-
-        // 헬스 체크 경로에 대한 추가적인 CORS 허용
-        registry.addMapping("/health")  // 헬스 체크 경로
-                .allowedOrigins("*")  // 모든 도메인 허용
-                .allowedMethods("GET")  // GET 요청만 허용
-                .allowedHeaders("*");
+                .allowCredentials(false);  // 자격 증명을 허용 (쿠키, 인증 헤더 전송)
     }
 }
