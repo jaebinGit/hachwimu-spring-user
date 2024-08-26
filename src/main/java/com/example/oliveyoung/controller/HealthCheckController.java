dd86c8fdc95f2707595e1fc04414b25f1a0f2e43
jaebinGit <tnwoql327@gmail.com>
2024-08-26 13:20:07 +0900

    Refactor : healthCheck 구현

---
+++ src/main/java/com/example/oliveyoung/controller/HealthCheckController.java
@@ -0,0 +1,22 @@
+package com.example.oliveyoung.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class HealthCheckController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
+
+    // /user/health 요청을 처리하는 메서드
+    @GetMapping("/health")
+    public String userHealthCheck() {
+        logger.info("User에서 health check를 받았습니다.");
+        return "User service is healthy!";
+    }
+}
